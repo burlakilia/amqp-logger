8048c1281017121416b60d5a0050e8811c47bbb9
Artem Burtsev <a.burtsev@sdventures.com>
2015-02-06 18:57:58 +0300

    Moved compose function to arguments

--- logger.js
+++ logger.js
@@ -1,21 +1,22 @@
-var gateway = require('owg'),
-    os = require('os'),
-    util = require('util');
+var gateway = require('owg');
 
 var levels = ['log', 'trace', 'debug', 'info', 'warn', 'error'];
-
-function transform(msg) {
-    return typeof msg === 'object' ? JSON.stringify(msg) : msg;
-}
 
 function compare(current, minimal) {
     return levels.indexOf(current) >= levels.indexOf(minimal);
 }
 
-exports.init = function (tracer, config) {
+exports.init = function (tracer, config, compose) {
     var publish = gateway(config, tracer).publish,
         cache = [],
         timeout;
+
+    function send () {
+        cache.forEach(publish);
+        tracer.trace('Messages were sent to kibana', cache);
+        cache = [];
+        timeout = null;
+    }
 
     function create(logger) {
         var cfg = config,
@@ -23,33 +24,17 @@ exports.init = function (tracer, config) {
 
         function wrap(level) {
 
-            function compose (msg) {
-                var message = util.format.apply(null, typeof msg === 'object' && msg.length ? msg.map(transform) : [transform(msg)]);
-
-                return {
-                    timestamp: new Date().toISOString(),
-                    logger: logger,
-                    level: level.charAt(0).toUpperCase() + level.substr(1),
-                    environment: cfg.environment,
-                    machine: os.hostname(),
-                    app: cfg.name,
-                    message: message
-                };
-
-            }
-
-            function send() {
-                cache.forEach(publish);
-                tracer.trace('Messages were sent to kibana', cache);
-                cache = [];
-                timeout = null;
-            }
-
-            fns[level] = function () {
+            fns[level] = function() {
                 var args = Array.prototype.slice.call(arguments);
 
+                var msg = {
+                    level: level,
+                    logger: logger,
+                    msg: util.format.apply(null, args.map(transform))
+                };
+
                 tracer[level].apply(tracer, args);
-                compare(level, cfg.level) && cache.push(compose(args));
+                compare(level, cfg.level) && cache.push(compose(msg));
 
                 timeout || (timeout = setTimeout(send, cfg.delay));
             };
